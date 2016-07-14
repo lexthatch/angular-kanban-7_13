55ec8d4c9a3c8bf3949096d64c46cb85552be92d
lexthatch <lexthatch@hotmail.com>
2016-07-13 19:09:31 -0600

    chat update
    added chat

--- services/chat.js
+++ services/chat.js
@@ -2,20 +2,20 @@
 
 	var app = angular.module('ngKanban');
 
-	app.factory('chatService', ['$rootScope', function ($rootScope) {
+	app.factory('chatService', ['$rootScope', '$q', function ($rootScope, $q) {
 
 		var conversations = [];
 
 		firebase.database().ref('chat/conversations/').on('value', function (snapshot) {
 
-if(snapshot.hasChildren()) {
-	conversations = [];
-}
+			if (snapshot.hasChildren()) {
+				conversations = [];
+			}
 
 			snapshot.forEach(function (childSnapshot) {
 				conversations.push(childSnapshot.val());
 			});
-console.log('Conversations', conversations)
+			console.log('Conversations', conversations)
 			$rootScope.$broadcast('userlist-updated', users);
 		});
 
@@ -24,10 +24,24 @@ console.log('Conversations', conversations)
 			return conversations;
 
 		}
-		
+
+		function getConversationMessages(conversationId) {
+			
+			var deferred = $q.defer();
+
+			firebase.database().ref('ref(chat/conversations' + conversationId).once('value', function (snapshot) {
+				var messages = [];
+
+				snapshot.forEach(function (childSnapshot) {
+					conversations.push(childSnapshot.val());
+				});
+				deferred.resolve(messages);
+			});
+			return deferred.promise;
+		}
 
 		return {
-		getConversations: getConversations
-	};
-}]);
+			getConversations: getConversations
+		};
+	}]);
 })();
